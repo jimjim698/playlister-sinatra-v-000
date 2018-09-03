9b7d07e9e315695c44462c1a2093070c50569b12
James Lotruglio <jimlotruglio@yahoo.com>
2018-09-03 03:25:29 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -34,7 +34,7 @@ get '/songs/:slug/edit' do
 end
 
 post '/songs/:slug' do
-  #raise params.inspect
+  raise params.inspect
   @song = Song.find_by_slug(params[:slug])
   @song.update(params[:song])
   @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
